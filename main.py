aa990a5d81fecaba40a4f14a505bba45ecc9c938
DZYT002 <135637449+DZYT002@users.noreply.github.com>
2023-07-21 02:13:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,14 +46,14 @@ else:
 
 
 #VersionChecker
-version='beta-0.6-pre1' #Dont Edit this!
+version='beta-0.6-pre2' #Dont Edit this!
 url = 'https://pastebin.com/raw/RmfvMed7'
 request_latest = requests.get(url)
 latest_version = request_latest.text
 if version==latest_version:
-    print(TerminalPrefix + " <|> This is a Pre-Release")
+    print(TerminalPrefix + " <|> This is a Pre-Release (Unstable)")
 else:
-    print(TerminalPrefix + " <|> This is a Pre-Release")
+    print(TerminalPrefix + " <|> This is a Pre-Release (Unstable)")
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
@@ -173,7 +173,26 @@ if __name__ == "__main__":
                     speek(wp_search)
                     break
 
+                if "note add" in listened:
+                    speek("What do you want to add?")
+                    listened_item=listen()
+                    print(listened_item)
+                    with open("notes.txt", "a") as addfile:
+                        addfile.write(str(listened_item) + "\n")
+                        break
 
+                if "note read" in listened:
+                    notes = []
+                    with open("notes.txt", "r") as file:
+                        lines = file.readlines()
+                        for line in lines:
+                            notes.append(str(line.strip()))
+                    if not notes:
+                        speek("You don't have any notes yet!")
+                    else:
+                        speek("Your notes are: ")
+                        speek(line)
+                        speek("if you want to add notes say 'vito add a note!'")
 
                 if shutdown in listened:
                     print(TerminalPrefix + " <|> " + shutdown)
